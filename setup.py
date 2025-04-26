27024a0bf3b3d26a0d8b92cdd87ce8eb00cf3a99
Vishnu Priya <vishnupriyadevi@gmail.com>
2025-04-26 10:25:33 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function returns list of requirements
+    '''
+    HYPEN_E_DOT='-e.'
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+            
+    return requirements
+        
+setup(
+    name='mlp',
+    version='0.0.1',
+    author='VP',
+    author_email='vishnupriyadevi7@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
